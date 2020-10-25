d8d4c30dc51af79a884c9a16352a59bb2e49d777
Sean Magaud <sean.magaud@easy.movie>
2020-10-26 00:30:56 +0100

    fix review

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.main')
-@section('title') cart @endsection
+@section('title') checkout @endsection
 @section('extra-css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
     <link rel="stylesheet" href="/css/checkout.css">
